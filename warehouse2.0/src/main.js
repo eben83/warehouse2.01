0c94d4c68a7ed22e0df1bfcfafe332a53ea99f4a
ebenBurger <99735780+ebenBurger@users.noreply.github.com>
2022-05-11 13:17:27 +0200

    main js  tweaks

--- warehouse2.0/src/main.js
+++ warehouse2.0/src/main.js
@@ -2,12 +2,23 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import {library} from '@fortawesome/fontawesome-svg-core'
+import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome'
+import {BootstrapVue, IconsPlugin} from "bootstrap-vue";
 import {faBars} from "@fortawesome/free-solid-svg-icons";
 
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.config.productionTip = false
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
+
+Vue.filter('dateFilter', function (value) {
+    if (!value) return ''
+    let localeValue = new Date(value)
+    return localeValue.toLocaleDateString("en-GB")
+})
 
 library.add(
     faBars
