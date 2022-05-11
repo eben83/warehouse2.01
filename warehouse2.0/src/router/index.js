02a78a99148c75de8e5e681d35994e0c5472c559
ebenBurger <99735780+ebenBurger@users.noreply.github.com>
2022-05-11 15:08:14 +0200

    routes updated

--- warehouse2.0/src/router/index.js
+++ warehouse2.0/src/router/index.js
@@ -2,26 +2,64 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import loginView from "@/views/loginView";
+import forgotPassword from "@/views/forgotPassword";
+import layoutView from "@/views/layoutView";
+import RegisterView from "@/views/registerView";
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'loginView',
-    component: loginView
+    name: 'login',
+    component: loginView,
+    meta: {
+      title: "Login"
+    },
   },
   {
-    path: '/home',
-    name: 'home',
-    component: HomeView
+    path: '/forgotPassword',
+    name: 'forgotPassword',
+    component: forgotPassword,
+    meta: {
+      title: "Forgot Password"
+    },
   },
+  {
+    path: '/',
+    name: 'layout',
+    component: layoutView,
+    children : [
+      {
+        path: '/home',
+        name: 'home',
+        component: HomeView,
+        meta: {
+          title: "Home"
+        },
+      },
+      {
+        path: '/register',
+        name: 'register',
+        component: RegisterView,
+        meta: {
+          title: "Register User"
+        },
+      },
+    ]
+  },
+
 ]
 
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  document.title = `${to.meta.title} | Warehouse`
+  next()
 })
 
 export default router
