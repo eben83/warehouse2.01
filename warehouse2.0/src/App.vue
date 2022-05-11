080a5bedd5390e408c3b1b4e342c1df19bfe0b7a
ebenBurger <99735780+ebenBurger@users.noreply.github.com>
2022-05-11 13:09:08 +0200

    app styling update, main FA added

--- warehouse2.0/src/App.vue
+++ warehouse2.0/src/App.vue
@@ -1,9 +1,5 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
     <router-view/>
   </div>
 </template>
@@ -15,6 +11,10 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  display: flex;
+  box-sizing: border-box;
+  min-height: 100vh;
+  overflow-x: hidden;
 }
 
 nav {
@@ -29,4 +29,19 @@ nav {
     }
   }
 }
+.link {
+  cursor: pointer;
+  text-decoration: none;
+  text-transform: uppercase;
+  color: black;
+}
+
+.link-light {
+  color: #ffffff;
+}
 </style>
+
+<script>
+export default {
+}
+</script>
--- warehouse2.0/src/main.js
+++ warehouse2.0/src/main.js
@@ -2,10 +2,19 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import {faBars} from "@fortawesome/free-solid-svg-icons";
+
 
 Vue.config.productionTip = false
 
-new Vue({
+library.add(
+    faBars
+)
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
+    new Vue({
   router,
   store,
   render: h => h(App)
