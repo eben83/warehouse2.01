f6401c890b6a34d954078d1f96ccb8cd5cdbbaf2
ebenBurger <99735780+ebenBurger@users.noreply.github.com>
2022-05-12 19:25:34 +0200

    added clientHome UI

---
+++ warehouse2.0/src/views/clientHome.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "clientHome",
+  data: () => ({}),
+  beforeCreate() {
+  },
+  created() {
+  },
+  beforeMount() {
+  },
+  mounted() {
+  },
+  beforeUpdate() {
+  },
+  updated() {
+  },
+  methods: {},
+  computed: {},
+}
+</script>
+
+<style scoped>
+</style>
