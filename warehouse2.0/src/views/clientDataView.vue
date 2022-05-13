2c71dccadea4ad32de35d84a2b52fc4f58ef4c29
ebenBurger <99735780+ebenBurger@users.noreply.github.com>
2022-05-13 17:02:16 +0200

    client data UI added

---
+++ warehouse2.0/src/views/clientDataView.vue
@@ -0,0 +1,27 @@
+<template>
+<div></div>
+</template>
+
+<script>
+export default {
+  name: "clientDataView",
+  data: () => ({}),
+  beforeCreate() {
+  },
+  created() {
+  },
+  beforeMount() {
+  },
+  mounted() {
+  },
+  beforeUpdate() {
+  },
+  updated() {
+  },
+  methods: {},
+  computed: {},
+}
+</script>
+
+<style scoped>
+</style>
