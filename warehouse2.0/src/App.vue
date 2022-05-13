b5238767b73e1a6700754ca74d72849aa3857bea
ebenBurger <99735780+ebenBurger@users.noreply.github.com>
2022-05-13 11:56:28 +0200

    global styles updated

--- warehouse2.0/src/App.vue
+++ warehouse2.0/src/App.vue
@@ -47,6 +47,73 @@ nav {
   color: red;
 }
 
+.row{
+  padding: 0;
+  margin: 0 0 0.25rem;
+}
+
+.card{
+  border: none;
+  border-radius: 12px;
+}
+
+.table th, .table td{
+  border-top: none;
+}
+
+.table {
+  border-bottom: 2px solid #dee2e6;
+}
+
+th{
+  color: #707070;
+}
+.menu-card-outline{
+  border: var(--supporting-teal) solid 2px;
+  h3{
+    color: var(--supporting-teal);
+  }
+}
+.menu-card-outline:hover{
+  background-color: var(--supporting-teal);
+  h3{
+    color: white;
+  }
+}
+h4{
+  font-size: 1rem;
+  font-weight: 700;
+}
+
+.btn-icon{
+  background-color: transparent;
+  border: none;
+  color: var(--text-primary);
+}
+.btn-icon:hover{
+  background-color: transparent;
+  border: none;
+  color: var(--text-primary);
+}
+
+td .row{
+  margin-bottom: 0;
+}
+
+.img-to-fit{
+  width: 100%;
+}
+
+.primary-background{
+  background-color: var(--background-primary);
+}
+
+h5{
+  font-size: 1rem;
+  font-weight: 700;
+  margin-bottom: 0;
+}
+
 </style>
 
 <script>
